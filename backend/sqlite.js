e48cf80bbc83a16e165e3b14fcb2c3c2a9d81591
G.v.Lasantha <126494665+Gvlasantha@users.noreply.github.com>
2023-09-20 14:05:55 +0530

    Update sqlite.js

--- backend/sqlite.js
+++ backend/sqlite.js
@@ -1,30 +1,85 @@
-const sqlite3 = require('sqlite3').verbose();
+const { getDbConnection, closeConnection } = require('./sqlite');
 
-let _DBConnection;
-
-const connectDatabase = async () => {
-
-  if (process.env.NODE_ENV === "test" || process.env.NODE_ENV === "test-backend") {
-      return new sqlite3.Database(":memory:", sqlite3.OPEN_READWRITE);
-  } else {
-    return new sqlite3.Database('./db.sqlite', sqlite3.OPEN_READWRITE);
-  }
-}
-
-const getDbConnection = async () => {
-  if (!_DBConnection) {
-    _DBConnection = await connectDatabase();
-  }
-  return _DBConnection;
+// Function to read all student data
+const readStudents = async () => {
+  const db = await getDbConnection();
+  return new Promise((resolve, reject) => {
+    db.all("SELECT * FROM students", (err, rows) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(rows);
+      }
+    });
+  });
 };
 
-const closeConnection = conn => {
-  if (conn) {
-    return conn.close();
-  }
+
+// Function to read information of a specified student
+const readStudentInfo = async (studentId) => {
+  const db = await getDbConnection();
+  return new Promise((resolve, reject) => {
+    db.get("SELECT * FROM students WHERE id = ?", [studentId], (err, row) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(row);
+      }
+    });
+  });
 };
+
+
+// Function to add a student
+const addStudent = async (studentData) => {
+  const db = await getDbConnection();
+  return new Promise((resolve, reject) => {
+    const { name, age, grade } = studentData;
+    db.run("INSERT INTO students (name, age, grade) VALUES (?, ?, ?)", [name, age, grade], (err) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve('Student added successfully');
+      }
+    });
+  });
+};
+
+
+// Function to update details of a specific student
+const updateStudent = async (studentId, updatedData) => {
+  const db = await getDbConnection();
+  return new Promise((resolve, reject) => {
+    const { name, age, grade } = updatedData;
+    db.run("UPDATE students SET name = ?, age = ?, grade = ? WHERE id = ?", [name, age, grade, studentId], (err) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve('Student updated successfully');
+      }
+    });
+  });
+};
+
+// Function to delete a specified student
+const deleteStudent = async (studentId) => {
+  const db = await getDbConnection();
+  return new Promise((resolve, reject) => {
+    db.run("DELETE FROM students WHERE id = ?", [studentId], (err) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve('Student deleted successfully');
+      }
+    });
+  });
+};
+
 
 module.exports = {
-  getDbConnection,
-  closeConnection
+  readStudents,
+  readStudentInfo,
+  addStudent,
+  updateStudent,
+  deleteStudent,
 };
